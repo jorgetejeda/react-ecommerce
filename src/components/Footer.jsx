e48d9e1c451fb930ab517f4266a3a21c0fa918ab
Jorge de Jesus Tejeda Bello <jtejeda@Jorges-MacBook-Pro.local>
2021-06-16 00:52:48 -0400

    Se creo el layout que estara usando la aplicacion

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/components/Footer.css';
+
+const Footer = () => (
+  <div className="Footer">
+    <p className="Footer-title">React E-commerce</p>
+    <p className="Footer-copy">Todos los Izquierdos Reservados</p>
+  </div>
+);
+
+export default Footer;
