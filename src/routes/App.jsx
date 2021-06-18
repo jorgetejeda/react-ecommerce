b50cd68c54ae6519a938da84c130145aa992e653
Jorge de Jesus Tejeda Bello <jtejeda@Jorges-MacBook-Pro.local>
2021-06-18 00:32:05 -0400

    Se creo la funcionalidad para agregar o eliminar del carrito de compras

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -9,20 +9,23 @@ import {
   NotFound,
 } from '../containers';
 import Layout from '../components/Layout';
+import AppProvider from '../context/AppContext';
 
 const App = () => (
-  <BrowserRouter>
-    <Layout>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route exact path="/checkout/information" component={Information} />
-        <Route exact path="/checkout/payment" component={Payment} />
-        <Route exact path="/checkout/success" component={Success} />
-        <Route component={NotFound} />
-      </Switch>
-    </Layout>
-  </BrowserRouter>
+  <AppProvider>
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/checkout" component={Checkout} />
+          <Route exact path="/checkout/information" component={Information} />
+          <Route exact path="/checkout/payment" component={Payment} />
+          <Route exact path="/checkout/success" component={Success} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+  </AppProvider>
 );
 
 export default App;
